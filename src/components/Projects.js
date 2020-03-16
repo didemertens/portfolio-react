d43887974e4d948b87b3d7ec068d3f6ef64d3b30
didemertens <d.g.j.mertens@gmail.com>
2020-03-16 12:59:28 +0000

    changed project descriptions

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -19,8 +19,9 @@ const Projects = () => {
               <h3>Git Together</h3>
               <p>This is a place for developers where they can post and comment on questions
               and chat with each other. After registering, users also automatically get a buddy
-            to promote further communication.</p>
-              <p>It was built in 1 week with React, React Hooks, Django REST and PostgreSQL.</p>
+              to promote further communication.</p>
+
+              <p>It was a solo project built in 1 week with React, React Hooks, Django REST and PostgreSQL.</p>
             </div>
           </a>
         </div>
@@ -31,9 +32,9 @@ const Projects = () => {
             <div className="project-details-text">
               <h3>Out and About</h3>
               <p>Out and About is a website to find free outdoor events. One of my main tasks was adding
-              the search and sorting functionality, making it possible for users to search for
-            events with the postcode, type of activity, date and time.</p>
-              <p>This was a group project lasting 8 days and we built it with the MERN stack</p>
+              the search and sorting functionality, making it possible for users to find
+              events with a postcode, type of activity, date and time.</p>
+              <p>This was a group project lasting 8 days and we built it with React, Express, React, Node.js and MongoDB (MERN).</p>
             </div>
           </a>
         </div>
@@ -44,8 +45,9 @@ const Projects = () => {
             <div className="project-details-text">
               <h3>Animated</h3>
               <p>Animated is a collection of animated films and series. Users can find, add and
-            comment on all of their favourite animations.</p>
-              <p>It took me 8 days to built using the MERN stack to create my own frontend and
+              comment on their favourite animations. For this app, I used MongoDB which was
+            a great experience as it stores all of the data in JSON. </p>
+              <p>It took me 1 week to built using the MERN stack to create my own frontend and
             API in the backend.</p>
             </div>
           </a>
@@ -57,7 +59,8 @@ const Projects = () => {
             <div className="project-details-text">
               <h3>Art Journey</h3>
               <p>Art Journey is a website on which users can search for art with a wide range
-            of keywords, such as "blue", "beach" and "Rembrandt".</p>
+              of keywords, such as "blue" and "Rembrandt". We took advantage of the CSS framework
+              Bulma to make styling the website easier and faster.</p>
               <p>This was a pair-programming project of 48 hours and was built with React and
             the Rijksmuseum API.</p>
             </div>
@@ -69,8 +72,9 @@ const Projects = () => {
             <img src={frogger} alt="Frogger" />
             <div className="project-details-text">
               <h3>Frogger</h3>
-              <p>I created my own version of the popular game Frogger in which users need
-            to get all 5 witches across the board, avoiding the different obstacles.</p>
+              <p>I created my own version of the game Frogger in which users need
+              to get all 5 witches across the board, avoiding the different obstacles. The player
+            uses the arrow keys to move and can pick from 3 difficulty levels</p>
               <p>It took me 9 days to built, using vanilla JavaScript, HTML and CSS</p>
             </div>
           </a>
