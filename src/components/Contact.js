56a69b4c043c9de2bbf4bd59b489f3baf214298f
didemertens <d.g.j.mertens@gmail.com>
2020-03-16 10:15:48 +0000

    added media queries

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,13 +1,23 @@
 import React from 'react'
+import { FaEnvelope, FaGithub, FaLinkedin } from "react-icons/fa"
 
 const Contact = () => (
   <section className="contact-section" id="contact">
     <div className="contact-circle">
       <div className="contact-list-section">
         <h2>Contact</h2>
-        <a href="mailto: d.g.j.mertens@gmail.com" target="_blank" rel="noopener noreferrer">d.g.j.mertens@gmail.com</a>
-        <a href="https://github.com/didemertens" target="_blank" rel="noopener noreferrer">github.com/didemertens</a>
-        <a href="https://linkedin.com/in/didemertens/" target="_blank" rel="noopener noreferrer">linkedin.com/in/didemertens</a>
+        <div className="contact-text-icon">
+          <FaEnvelope className="contact-icon" />
+          <a href="mailto: d.g.j.mertens@gmail.com" target="_blank" rel="noopener noreferrer">d.g.j.mertens@gmail.com</a>
+        </div>
+        <div className="contact-text-icon">
+          <FaGithub className="contact-icon" />
+          <a href="https://github.com/didemertens" target="_blank" rel="noopener noreferrer">github.com/didemertens</a>
+        </div>
+        <div className="contact-text-icon">
+          <FaLinkedin className="contact-icon" />
+          <a href="https://linkedin.com/in/didemertens/" target="_blank" rel="noopener noreferrer">linkedin.com/in/didemertens</a>
+        </div>
       </div>
     </div>
   </section>
